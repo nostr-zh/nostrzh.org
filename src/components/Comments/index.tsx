4ac88f5e138de6b62dffc05f53e3bc38315d1fbc
codytseng <codytseng98@gmail.com>
2026-01-30 23:22:10 +0800

    feat: 替换评论组件

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,17 @@
+import { NostrComments } from "nostr-comments";
+
+interface CommentsProps {
+  articleSlug: string;
+  articleAuthorPubkeys?: string[];
+}
+
+export default function Comments({
+  articleSlug,
+  articleAuthorPubkeys,
+}: CommentsProps) {
+  const url = `https://nostrzh.org/blog/${articleSlug}`;
+
+  return (
+    <NostrComments url={url} locale="zh" authorPubkeys={articleAuthorPubkeys} />
+  );
+}
